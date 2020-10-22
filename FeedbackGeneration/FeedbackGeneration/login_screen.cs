e15f222335cafb51c1f9aa5ca26a64a98bd22a01
Alistair Brazier <AB2128@student.aru.ac.uk>
2020-10-22 09:20:03 +0100

    First Initial Commit

---
+++ FeedbackGeneration/FeedbackGeneration/login_screen.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FeedbackGeneration
+{
+    public partial class login_screen : Form
+    {
+        public login_screen()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ FeedbackGeneration/FeedbackGeneration/main_menu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FeedbackGeneration
+{
+    public partial class main_menu : Form
+    {
+        public main_menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_menu_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
